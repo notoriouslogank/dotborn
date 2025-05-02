bc6dcd7e9a80c0c5bccec240c1c828f1c8b08844
notoriouslogank <logan.aker1989@gmail.com>
2025-05-02 15:11:08 -0400

    feat(logger): add centralized Rich-based logging configuration (logger.py)

---
+++ dotborn/logger.py
@@ -0,0 +1,31 @@
+import logging
+from pathlib import Path
+from rich.logging import RichHandler
+from dotborn import paths
+
+def setup_logger(name:str=__name__, level=logging.DEBUG, verbose:bool=True, quiet=False, log_file: Path=paths.LOG_PATH) -> logging.Logger:
+    if quiet:
+        level = logging.WARNING
+    elif verbose:
+        level = logging.DEBUG
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.propagate = False
+
+    if not logger.handlers:
+        # Console Handler
+        rich_handler = RichHandler(rich_tracebacks=True, markup=True, show_time=True, show_level=True, show_path=True)
+        rich_formatter=logging.Formatter(">> %(message)s")
+        rich_handler.setFormatter(rich_formatter)
+        logger.addHandler(rich_handler)
+
+        # File Handler
+        file_handler = logging.FileHandler(log_file)
+        file_formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        file_handler.setFormatter(file_formatter)
+        file_handler.setLevel(level)
+        logger.addHandler(file_handler)
+
+    return logger
+
