1e29df297dcc2ea8cccf14544f13156b37d7e5f8
notoriouslogank <logan.aker1989@gmail.com>
2025-05-02 15:13:13 -0400

    feat(hash): implement hashing utilities for file integrity (hash.py)

---
+++ dotborn/hash.py
@@ -0,0 +1,16 @@
+import hashlib
+from pathlib import Path
+from utils.logger import setup_logger
+
+log = setup_logger()
+
+def hash_file(path: Path, algo="sha256"):
+    log.info(f"Hashing {path}....")
+    h = hashlib.new(algo)
+    with open(path, 'rb') as f:
+        while chunk := f.read(8192):
+            h.update(chunk)
+    hash = h.hexdigest()
+    log.info(f"Hash: {hash}")
+    return hash
+
