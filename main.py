76d4c73c631b5dfe1ef22d072a7961b8f45151f9
notoriouslogank <logan.aker1989@gmail.com>
2025-05-02 15:10:04 -0400

    feat(core): add entry point script for dotborn CLI (main.py)

---
+++ main.py
@@ -0,0 +1,38 @@
+from dotborn.version import get_version
+import sys # these lines are for DEV ONLY and should be
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.resolve()))           # fixed later per ChatGPT
+from dotborn import logger, paths, config, platform_check, linback, apt_installer
+
+log = logger.setup_logger(log_file=Path(paths.LOG_PATH))
+
+conf = config.load_config()
+
+def main():
+    log.debug(f"MAIN LOOP START")
+    print(f'Dotborn v{get_version()}')
+    exit()
+    platform = platform_check.check_platform()
+
+    if platform == "Windows":
+        print(f"Windows")
+    if platform == "Linux":
+        #linback.run_backup()
+        apt_installer.install_apt_packages(conf)
+        apt_installer.install_cargo_packages(conf)
+        apt_installer.install_script_packages(conf)
+    else:
+        print(f"Some sort of Godless heathen")
+
+
+    # backup
+    #print(backup.copy_windows_credentials())
+    #print(backup.copy_windows_browser_data())
+    #print(backup.copy_windows_ect())
+    #make_tempdir()
+    # install
+    # dotfiles
+    # post-install
+    #pass
+
+main()
