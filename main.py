cd09b1e145d4ab683b36fb832119fa3004e3d6ad
notoriouslogank <logan.aker1989@gmail.com>
2025-05-05 13:20:33 -0400

    style: formatted with black

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import sys  # these lines are for DEV ONLY and should be
+import sys
 from pathlib import Path
 
 from dotborn import logger, platform_check
 from dotborn.backupper import BackupManager, WinBack, LinBack
-from dotborn.installer import (AptInstaller, CargoInstaller, InstallManager,
-                               ScriptInstaller)
+from dotborn.installer import (
+    AptInstaller,
+    CargoInstaller,
+    InstallManager,
+    ScriptInstaller,
+)
 from dotborn.version import get_version
 from dotborn.config import Configure
 
 configs = Configure()
 
-sys.path.append(
-        str(Path(
-            __file__
-            ).parent.resolve()))  # fixed later per ChatGPT
+sys.path.append(str(Path(__file__).parent.resolve()))  # fixed later per ChatGPT
 
 log = logger.setup_logger(log_file=Path(configs.LOG_PATH))
+
 
 def main():
     log.debug(f"dotborn {get_version()}")
@@ -25,30 +27,29 @@ def main():
     log.debug(f"User platform: {platform}")
     backup_manager = BackupManager(configs.user_config, configs.backup_config)
 
-
     if platform == "Windows":
         winconfigs = backup_manager.prepare_windows()
         winback = WinBack(winconfigs)
         winback.backup()
     if platform == "Linux":
         linconfigs = backup_manager.prepare_linux()
-        #print(linconfigs)
+        # print(linconfigs)
         linback = LinBack(linconfigs)
         linback.backup()
 
-#        install_manager = InstallManager(configs.user_config, configs.install_config)
-#        apt_installer = AptInstaller(
-#        install_manager.apt_list,
-#        install_manager.flags)
-#        cargo_installer = CargoInstaller(
-#            install_manager.cargo_list,
-#            install_manager.flags)
-#        script_installer = ScriptInstaller(
-#            install_manager.script_list,
-#            install_manager.flags)
-#        apt_installer.dry_run()
-#        cargo_installer.dry_run()
-#        script_installer.dry_run()
+    #        install_manager = InstallManager(configs.user_config, configs.install_config)
+    #        apt_installer = AptInstaller(
+    #        install_manager.apt_list,
+    #        install_manager.flags)
+    #        cargo_installer = CargoInstaller(
+    #            install_manager.cargo_list,
+    #            install_manager.flags)
+    #        script_installer = ScriptInstaller(
+    #            install_manager.script_list,
+    #            install_manager.flags)
+    #        apt_installer.dry_run()
+    #        cargo_installer.dry_run()
+    #        script_installer.dry_run()
     else:
         log.debug(f"Unsupported platform: {platform}")
 
