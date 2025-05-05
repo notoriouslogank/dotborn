5c8b47abc3daea61c6c33f7502e663547c7e7ef4
notoriouslogank <logan.aker1989@gmail.com>
2025-05-05 13:18:54 -0400

    style: formatted with black

--- dotborn/logger.py
+++ dotborn/logger.py
@@ -5,7 +5,14 @@ from dotborn.config import Configure
 
 configs = Configure()
 
-def setup_logger(name:str=__name__, level=logging.INFO, verbose:bool=False, quiet=False, log_file: Path=configs.LOG_PATH) -> logging.Logger:
+
+def setup_logger(
+    name: str = __name__,
+    level=logging.INFO,
+    verbose: bool = False,
+    quiet=False,
+    log_file: Path = configs.LOG_PATH,
+) -> logging.Logger:
     if quiet:
         level = logging.WARNING
     elif verbose:
@@ -17,17 +24,25 @@ def setup_logger(name:str=__name__, level=logging.INFO, verbose:bool=False, quie
 
     if not logger.handlers:
         # Console Handler
-        rich_handler = RichHandler(rich_tracebacks=True, markup=True, show_time=True, show_level=True, show_path=True)
-        rich_formatter=logging.Formatter(">> %(message)s")
+        rich_handler = RichHandler(
+            rich_tracebacks=True,
+            markup=True,
+            show_time=True,
+            show_level=True,
+            show_path=True,
+        )
+        rich_formatter = logging.Formatter(">> %(message)s")
         rich_handler.setFormatter(rich_formatter)
         logger.addHandler(rich_handler)
 
         # File Handler
         file_handler = logging.FileHandler(log_file)
-        file_formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        file_formatter = logging.Formatter(
+            "[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
         file_handler.setFormatter(file_formatter)
         file_handler.setLevel(level)
         logger.addHandler(file_handler)
 
     return logger
-
