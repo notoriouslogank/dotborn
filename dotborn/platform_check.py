6b2f01e6c0820a603934df9884b859e846a2542d
notoriouslogank <logan.aker1989@gmail.com>
2025-05-05 13:19:14 -0400

    style: formatted with black

--- dotborn/platform_check.py
+++ dotborn/platform_check.py
@@ -3,9 +3,9 @@ from dotborn import logger
 
 log = logger.setup_logger()
 
+
 def check_platform():
     log.debug(f"Checking platform...")
     system = platform.system()
     log.info(f"Platform detected: {system}")
     return system
-
