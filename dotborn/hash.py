154beb2c33ac35df7c073f42b7d12db61678b202
notoriouslogank <logan.aker1989@gmail.com>
2025-05-05 13:17:35 -0400

    style: formatted with black formatter

--- dotborn/hash.py
+++ dotborn/hash.py
@@ -4,13 +4,13 @@ from dotborn.logger import setup_logger
 
 log = setup_logger()
 
+
 def hash_file(path: Path, algo="sha256"):
     log.info(f"Hashing {path}....")
     h = hashlib.new(algo)
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         while chunk := f.read(8192):
             h.update(chunk)
     hash = h.hexdigest()
     log.info(f"Hash: {hash}")
     return hash
-
